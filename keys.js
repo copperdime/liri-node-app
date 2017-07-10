dc66887fd351a08cf29ef6b906086a30b08727b7
copperdime <mrs.kelly@gmail.com>
2017-07-09 22:14:48 -0700

    first version of the liri app

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 
-exports.twitterKeys = {
-  consumer_key: 'RBsieTGlxofNEgwvw7TivJDGX',
-  consumer_secret: 'JltSR59DKRkMDeWeIP7jCMWKW6vmGBUVMl7ukzfOhjtAwLvZtN',
-  access_token_key: '880593295327363072-DM6wGeAvUE3PUDpIJrao911xuV5e52f',
-  access_token_secret: 'MrQpIRAEdjEC8AmM2H0xVSPKOByvGjms89QxyDMp28KFt',
-}
+exports.keysForTwitter = {
+  consumer_key: 'QD6jBts1yS8H5FnAJs1hFj1mA',
+  consumer_secret: 'IvVFKPgL58lsN94IN5c6qLgkb9I7J8r3QpFZO1QlxeHUSj9nxK',
+  access_token_key: '884190618469126145-ZfFdrO3jfkr2OWaKBUJXAjBqvcIjSUp',
+  access_token_secret: 'CiSHX51uJgmL58fWORSjEKLzSBNGP2eic54tV7cXyKvZ8',
+};
---
+++ spotifyKeys.js
@@ -0,0 +1,5 @@
+
+exports.keysForSpotify = {
+    id: '5043de56c78d4bd488d66f9dbd0fe9a6',
+    secret: 'cb9d05bc06b0409cb5f4fe5fe53fc8c8'
+};
